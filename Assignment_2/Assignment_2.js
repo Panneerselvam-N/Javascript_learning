63d671e72f4b1c34f650fefa031ebb653229d89b
Npsking <npsking15ee16@gmail.com>
2021-08-05 11:59:49 +0530

    Initial commit

---
+++ Assignment_2/Assignment_2.js
@@ -0,0 +1,18 @@
+
+// Tash -1
+let task_1 = ['Iron_man','Thor','Captain','Thanos','Hulk'];
+console.log('Task - 1')
+console.log('Marvel characters')
+for(let i =0;i<task_1.length;i++)
+{
+    console.log(`Marvel characters are ${task_1[i]}`);
+
+}
+
+console.log('Tast - 2')
+console.log('Numbers devisible by 5')
+
+for(let i=0; i<=50 ;i++){
+    if(i%5 == 0){
+         console.log(i)}
+}
---
+++ Assignment_3/Assignment_3.js
@@ -0,0 +1,9 @@
+
+
+
+function Add(){
+    let val_1 = document.querySelector('#name').value 
+    let val_2 = document.querySelector('#city').value 
+   document.querySelector('#dis').innerHTML=`<h3>${val_1} from ${val_2}</h3>`
+
+}
